0fbc0d6774daf2b655f202136482f16279e1d043
dog <komstroj3@mail.ru>
2024-12-12 02:35:53 +0300

    add pulse_width value, fix pwm period

--- src/main.rs
+++ src/main.rs
@@ -40,22 +40,37 @@ fn main() -> ! {
         .pwm_hz::<Tim2NoRemap, _, _>(pwm_pin, &mut afio.mapr, 50.Hz(), &clocks);
     ////////////////////////////////////////////////////////////////
 
-    pwm_timer.enable(Channel::C1);
-    // let max_duty = pwm_timer.get_max_duty();
-    // pwm_timer.set_duty(Channel::C1, max_duty/4);
-    // pwm_timer.set_period(ms(2).into_rate());
-    // pwm_timer.set_duty(Channel::C1, 0);
-    // pwm_timer.set_duty(Channel::C1, 2700);
-    // cortex_m::asm::delay(10_000_000);
-    // pwm_timer.set_duty(Channel::C1, 0);
-    
-    //?????
-    // dp.TIM2.ccr1.modify(|_, w| unsafe { w.bits(config - 1) });
+    // Сервопривод работает при 50 герц, которые мы поставили на таймере TIM2
+    // Частота таймера 50 герц == Период цикла шим 20 милисекунд
+    // Если поставить ширину импульса на 1-2 милисекунды это изменит положение
+    let servo_period = 20_000;
+    let pulse_width_0deg = 1000; // Микросекунды (0 градусов) 2667
+    let pulse_width_90deg = 1500; // Микросекунды (90 градусов) 4000
+    let pulse_width_180deg = 2000; // Микросекунды (180 градусов) 5333
 
+    // Рассчет значений для set_duty (важно для точности!) 
+    // TODO: Использовать Decimal вместо float???
+    // TODO: Посмотреть точные значение для пульсации по градусам
+    // TODO: Какое точное значение у pwm_timer.get_max_duty(), меняется ли оно 
+    // или необходимо все сместить к float???
+    let duty_0deg = 2667; //(pulse_width_0deg/servo_period)*pwm_timer.get_max_duty();
+    let duty_90deg = 5333; //(pulse_width_90deg/servo_period)*(pwm_timer.get_max_duty() as f16);
+    let duty_180deg = 7330; //(pulse_width_180deg/servo_period)*(pwm_timer.get_max_duty() as f16);
+
+    pwm_timer.enable(Channel::C1); // Включаем канал
+    pwm_timer.set_period(ms(20).into_rate());
+    
     loop {
         block!(timer.wait()).unwrap();
         led.set_low();        
         block!(timer.wait()).unwrap();
         led.set_high();
+
+        pwm_timer.set_duty(Channel::C1, duty_0deg);
+        cortex_m::asm::delay(10_000_000);
+        pwm_timer.set_duty(Channel::C1, duty_90deg);
+        cortex_m::asm::delay(10_000_000);
+        pwm_timer.set_duty(Channel::C1, duty_180deg);
+        cortex_m::asm::delay(10_000_000);
     }
 }
